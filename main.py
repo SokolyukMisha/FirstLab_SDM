429f0d81e353c30b8ea9651756ff7476e391d178
SokolyukMisha <sokolyukmisha@gmail.com>
2022-02-18 23:21:00 +0200

    Interactive code
    now it can interacttive solve the equation

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ from math import sqrt
 
 def Discriminant():
     a = float(input("a = "))
+    if a == 0:
+        print("a can't be zero")
+        Discriminant()
+        return
     b = float(input("b = "))
     c = float(input("c = "))
-    discriminant = pow(b, 2) - (4 * a * c)
-    return print(discriminant)
+    d = pow(b, 2) - (4 * a * c)
+    return Solve(a,b,c,d)
 
+def Solve(a, b,c, d):
+    print(f"({a}) x^2 + ({b}) x + ({c}) = 0")
+    if d > 0:
+        x1 = (-b + sqrt(d)) / (2 * a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        print("There are 2 roots")
+        print(f"x1 = {x1}, x2 = {x2}")
+    elif d == 0:
+        x1 = -(b / (2 * a))
+        print("There are 1 root")
+        print(f"x = {x1}")
+    else:
+        print("There are 0 roots")
 
-Discriminant() 
+def Interactive():
+        Discriminant()
+
+Interactive()
